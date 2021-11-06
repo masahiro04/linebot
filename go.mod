311d7fcf3d62ce8b3cf4e338561d07ef7b0c8aaa
masahirookubo <stmhamachiii@gmail.com>
2021-11-06 13:02:55 +0900

    メッセージ処理の追加

--- go.mod
+++ go.mod
@@ -6,7 +6,10 @@ require (
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/line/line-bot-sdk-go v7.8.0+incompatible
+	github.com/line/line-bot-sdk-go/v7 v7.11.0 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/ugorji/go v1.2.6 // indirect
--- main.go
+++ main.go
@@ -1,18 +1,102 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(gin.Logger())
+	bot, err := linebot.New(
+		os.Getenv("LINE_SECRET_KEY"),
+		os.Getenv("LINE_ACCESS_TOKEN"),
+	)
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "hello",
 		})
 	})
 
-	r.Run()
+	r.POST("/callback", func(c *gin.Context) {
+		events, err := bot.ParseRequest(c.Request)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				log.Print(err)
+			}
+			return
+		}
+
+		// "可愛い" 単語を含む場合、返信される
+		var replyText string
+		replyText = "可愛い"
+
+		// チャットの回答
+		var response string
+		response = "ありがとう！！"
+
+		// "おはよう" 単語を含む場合、返信される
+		var replySticker string
+		replySticker = "おはよう"
+
+		// スタンプで回答が来る
+		responseSticker := linebot.NewStickerMessage("11537", "52002757")
+
+		// "猫" 単語を含む場合、返信される
+		var replyImage string
+		replyImage = "猫"
+
+		// 猫の画像が表示される
+		responseImage := linebot.NewImageMessage(
+			"https://i.gyazo.com/2db8f85c496dd8f21a91eccc62ceee05.jpg",
+			"https://i.gyazo.com/2db8f85c496dd8f21a91eccc62ceee05.jpg",
+		)
+
+		// "ディズニー" 単語を含む場合、返信される
+		var replyLocation string
+		replyLocation = "ディズニー"
+
+		// ディズニーが地図表示される
+		responseLocation := linebot.NewLocationMessage("東京ディズニーランド", "千葉県浦安市舞浜", 35.632896, 139.880394)
+
+		for _, event := range events {
+			// イベントがメッセージの受信だった場合
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				// メッセージがテキスト形式の場合
+				case *linebot.TextMessage:
+					replyMessage := message.Text
+					// テキストで返信されるケース
+					if strings.Contains(replyMessage, replyText) {
+						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(response)).Do()
+						// スタンプで返信されるケース
+					} else if strings.Contains(replyMessage, replySticker) {
+						bot.ReplyMessage(event.ReplyToken, responseSticker).Do()
+						// 画像で返信されるケース
+					} else if strings.Contains(replyMessage, replyImage) {
+						bot.ReplyMessage(event.ReplyToken, responseImage).Do()
+						// 地図表示されるケース
+					} else if strings.Contains(replyMessage, replyLocation) {
+						bot.ReplyMessage(event.ReplyToken, responseLocation).Do()
+					}
+					// 上記意外は、おうむ返しで返信
+					_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+					if err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+	})
+
+	err = r.Run()
+	if err != nil {
+		return
+	}
 }
